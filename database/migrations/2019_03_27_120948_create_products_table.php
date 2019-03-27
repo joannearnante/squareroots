01f5fc197a5f4d01b5aef1e6a842e44b1a7ef573
Joanne Arnante <arnante.joanne@gmail.com>
2019-03-27 20:24:13 +0800

    added some tables

---
+++ database/migrations/2019_03_27_120948_create_products_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->text("description");
+            $table->decimal('price', 10, 2);
+            $table->decimal('stocks', 3, 0);
+            $table->string("img_path");
+            $table->string('isActive')->default('true');
+            $table->unsignedBigInteger('category_id');
+            $table->timestamps();
+            //relate categories and items
+            $table->foreign('category_id')
+            ->references('id')
+            ->on('categories')
+            ->onDelete("restrict")
+            ->onUpdate("cascade");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
