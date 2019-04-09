2c19c605877cc61b5dcc066ffb53eccd3a8f2ef0
joannearnante <arnante.joanne@gmail.com>
2019-04-09 10:59:27 +0800

    fix

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,19 +5,19 @@
 <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
 <div class="carousel-inner">
 <div class="carousel-item active">
-  <img class="d-block w-100" src="{{URL::asset('/images/carousel3.jpg')}}" alt="First slide">
+  <img class="d-block w-100" src="{{URL::asset('/images/1.png')}}" alt="First slide">
   <div class="carousel-caption {{-- d-none --}} d-flex justify-content-start text-left" style="top: 50%; left: 5%;">
     <h1 class="display-2 text-dark">Nourish the body<br>and soul</h1>
   </div>
 </div>
 <div class="carousel-item">
-  <img class="d-block w-100" src="{{URL::asset('/images/carousel2.jpg')}}" alt="Second slide">
+  <img class="d-block w-100" src="{{URL::asset('/images/2.png')}}" alt="Second slide">
   <div class="carousel-caption {{-- d-none --}} d-flex justify-content-start text-left" style="top: 50%; left: 5%;">
     <h1 class="display-2">Local produce<br>within reach</h1>
   </div>
 </div>
 <div class="carousel-item">
-  <img class="d-block w-100" src="{{URL::asset('/images/carousel1.jpg')}}" alt="Third slide">
+  <img class="d-block w-100" src="{{URL::asset('/images/3.png')}}" alt="Third slide">
   <div class="carousel-caption {{-- d-none --}} d-flex justify-content-start text-left" style="top: 50%; left: 5%;">
     <h1 class="display-2">Bridging technology <br>and farmers</h1>
 </div>
