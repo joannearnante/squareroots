91883415498295d69d676747b210a287ef937d9e
joannearnante <arnante.joanne@gmail.com>
2019-04-10 12:23:09 +0800

    major changes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,24 +2,24 @@
 
 @section('content')
 
-<div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+<div id="carouselExampleControls" class="carousel slide carousel-fade" data-ride="carousel">
 <div class="carousel-inner">
 <div class="carousel-item active">
   <img class="d-block w-100" src="{{URL::asset('/images/1.png')}}" alt="First slide">
-  <div class="carousel-caption {{-- d-none --}} d-flex justify-content-start text-left" style="top: 50%; left: 5%;">
-    <h1 class="display-2 text-dark">Nourish the body<br>and soul</h1>
+  <div class="carousel-caption {{-- d-none --}} d-flex justify-content-start text-left" style="top: 40%; left: 5%;">
+    <p class="text-dark captiontext">Nourish the body<br>and soul</p>
   </div>
 </div>
 <div class="carousel-item">
   <img class="d-block w-100" src="{{URL::asset('/images/2.png')}}" alt="Second slide">
-  <div class="carousel-caption {{-- d-none --}} d-flex justify-content-start text-left" style="top: 50%; left: 5%;">
-    <h1 class="display-2">Local produce<br>within reach</h1>
+  <div class="carousel-caption {{-- d-none --}} d-flex justify-content-start text-left" style="top: 40%; left: 5%;">
+    <p class="captiontext">Local produce<br>within reach</p>
   </div>
 </div>
 <div class="carousel-item">
   <img class="d-block w-100" src="{{URL::asset('/images/3.png')}}" alt="Third slide">
-  <div class="carousel-caption {{-- d-none --}} d-flex justify-content-start text-left" style="top: 50%; left: 5%;">
-    <h1 class="display-2">Bridging technology <br>and farmers</h1>
+  <div class="carousel-caption {{-- d-none --}} d-flex justify-content-start text-left" style="top: 40%; left: 5%;">
+    <p class="captiontext">Bridging technology <br>and farmers</p>
 </div>
 </div>
 <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Square Roots') }}</title>
+    <title>Square Roots-local produce from farm to table</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
@@ -15,8 +15,10 @@
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
 
     <!-- Styles -->
+    <link href="{{ asset('css/style.css') }}" rel="stylesheet">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <!-- Fontawesome -->
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
